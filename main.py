75fa7046feb574173faa7a99677e71010d5a3773
0xIrakli <butu.irakli@gmail.com>
2022-10-19 17:41:55 +0400

    gotta go back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-from numpy import full, ndarray
 import pygame
 import random as r
 from scipy.interpolate import interp1d as map_range
@@ -8,13 +7,13 @@ pygame.init()
 
 disp = pygame.display
 
-METER = 10000
+METER = 100 #1meter = 10000 pixels
 K = 9 * (10**9)
-MIN_DISTANCE = 0.01
+MIN_DISTANCE = 0.005
 Q = 10 * (10**(-9))
 M = 0.01
-PARTICLE_COUNT = 50
-FULLSCREEN = True
+PARTICLE_COUNT = 20
+FULLSCREEN = False
 DRAW_LINES = True
 
 if FULLSCREEN:
@@ -22,12 +21,15 @@ if FULLSCREEN:
 else:
     win = disp.set_mode((900, 900))    
 draw = pygame.draw
-
 disp.set_caption('MATEMATIKA MID')
+
 class Vector:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+    
+    def __str__(self) -> str:
+        return f"Vector({self.x}, {self.y})"
     
     def divide(self, X: float):
         return Vector(self.x / X, self.y / X)
@@ -35,22 +37,36 @@ class Vector:
     def mult(self, X: float):
         return Vector(self.x * X, self.y * X)
     
+    def to_pixels(self):
+        return self.mult(METER)
+
+    def to_meters(self):
+        return self.divide(METER)
+    
+    @property
+    def tuple(self) -> tuple:
+        return (self.x, self.y)
+    
     @property
     def mag(self) -> float:
         return abs(math.sqrt((self.x**2) + (self.y**2)))
     
     @staticmethod
     def distance_x(v1, v2) -> float:
-        return (v2.x-v1.x)/METER
+        dist = v2.x-v1.x
+        f = math.copysign(max(abs(dist), MIN_DISTANCE), dist)
+        return f
     
     @staticmethod
     def distance_y(v1, v2) -> float:
-        return (v2.y-v1.y)/METER
+        dist = v2.y-v1.y
+        f = math.copysign(max(abs(dist), MIN_DISTANCE), dist)
+        return f
     
     @staticmethod
     def distance(v1, v2) -> float:
-        f = math.sqrt((Vector.distance_x(v1, v2)**2)+(Vector.distance_y(v1, v2)**2))
-        return max(abs(f), MIN_DISTANCE)
+        distance = math.sqrt((Vector.distance_x(v1, v2)**2)+(Vector.distance_y(v1, v2)**2))
+        return max(distance, MIN_DISTANCE)
     
     @staticmethod
     def random(x1, x2, x3='X', x4='X'):
@@ -62,26 +78,33 @@ class Particle:
     acc = Vector(0, 0)
     vel = Vector(0, 0)
     colors = [(255, 51, 51), (51, 255, 51)]
+    forces = []
     
     #Random position, sign and q charge if not passed as parameters.
     def __init__(self, pos=0, sign=0, q=0, m=M) -> None:
-        self.pos = Vector.random(150, win.get_width()-150, 150, win.get_height()-150) if pos == 0 else pos
+        self.pos = Vector.random(200, win.get_width()-200, 200, win.get_height()-200) if pos == 0 else pos
+        self.pos = self.pos.to_meters()
         self.sign = r.choice([1, -1]) if sign == 0 else sign
         self.q = r.randrange(5, 20)*(10**(-9)) if q == 0 else q
         self.q = abs(self.q)*self.sign
-        print(self.sign)
         self.m = m
 
     def update(self) -> None:
+        #Some kind of drag so particles dont go crazy
+        self.vel.x *= 0.92
+        self.vel.y *= 0.92
+        
         self.vel.x += self.acc.x
         self.vel.y += self.acc.y
-        self.vel.x *= 0.95
-        self.vel.y *= 0.95
+        
         self.pos.x += self.vel.x
         self.pos.y += self.vel.y
         
+        #print('\n'.join(map(str, self.forces)), end='\n__________________________________\n')
+        self.forces = []
         self.acc = Vector(0, 0)
-        draw.circle(win, self.colors[0] if self.sign < 0 else self.colors[1], (self.x, self.y), 6)#abs(self.q)*(10**8.8))
+        print(self.pos.to_pixels().x)
+        draw.circle(win, self.colors[0] if self.sign < 0 else self.colors[1], self.pos.to_pixels().tuple, 4)#abs(self.q)*(10**8.8))
     
     @property
     def x(self) -> float:
@@ -93,6 +116,7 @@ class Particle:
     
     @staticmethod
     def apply_force(p, force) -> None:
+        p.forces.append(force)
         p.acc.x += force.x/p.m
         p.acc.y += force.y/p.m
         
@@ -101,8 +125,9 @@ class Particle:
         q1q2 = abs(p1.q)*abs(p2.q)
         dist = Vector.distance(p1.pos, p2.pos)
         
-        force_mag = ( q1q2 / (dist**2) ) * K
-        forcex, forcey = force_mag*( Vector.distance_x(p1, p2)/dist ), force_mag*( Vector.distance_y(p1, p2)/dist )
+        force_scalar = ( q1q2 / (dist**2) ) * K
+        forcex, forcey = force_scalar*( Vector.distance_x(p1, p2)/dist ), force_scalar*( Vector.distance_y(p1, p2)/dist )
+        #print(dist)
         
         if p1.sign == p2.sign:
             return Vector(-forcex, -forcey)
@@ -114,7 +139,7 @@ particles = [
 ]
 #Textbook example
 #particles = [
-#    Particle(Vector(400, 500), sign=-1, q=20*(10**(-9))),
+#    Particle(Vector(500, 500), sign=-1, q=Q),#20*(10**(-9))),
 #    Particle(Vector(600, 500), sign=1, q=Q),
 #    Particle(Vector(700, 500), sign=-1, q=Q),
 #]
@@ -126,15 +151,17 @@ while True:
             
     for i in range(len(particles)):
         for j in range(len(particles)):
+            if i == j:
+                continue
+            
             p1 = particles[i]
             p2 = particles[j]
 
-            force1 = Particle.calc_force(p1, p2)
-            Particle.apply_force(p1, force1)
+            force = Particle.calc_force(p1, p2)
+            Particle.apply_force(p1, force)
 
             if DRAW_LINES:
-                draw.line(win, (150, 150, 150), (p1.x, p1.y), (p2.x, p2.y), max(1, min(3, round(force1.mag*4000))))
-    
+                draw.line(win, (150, 150, 150), (p1.pos.to_pixels().x, p1.pos.to_pixels().y), (p2.pos.to_pixels().x, p2.pos.to_pixels().y), max(1, min(3, round(force.mag*400))))
     [p.update() for p in particles]
     disp.update()
     
